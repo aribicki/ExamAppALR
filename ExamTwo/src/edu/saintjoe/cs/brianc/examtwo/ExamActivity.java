91742150044b2a78472fea20ab63072f1d8b57f6
Andrew <aribicki@gmail.com>
2015-04-08 16:02:07 -0400

    Made it double, removed useless code, and added comments.

--- ExamTwo/src/edu/saintjoe/cs/brianc/examtwo/ExamActivity.java
+++ ExamTwo/src/edu/saintjoe/cs/brianc/examtwo/ExamActivity.java
@@ -16,53 +16,51 @@ import com.google.devtools.simple.runtime.events.EventDispatcher;
 
 public class ExamActivity extends Form implements HandlesEventDispatching {
 
-	private HorizontalArrangement line1, line2, line3, line4, line5;
-	private Button incButton;
-	private Label resultLabel;
-	private Label promptLabel;
-	private TextBox inputBox;
-	private Label outputLabel;
+	private HorizontalArrangement line1, line2, line3; // calls for horizontal arangement component
+	private Button incButton; // allows button component
+	private Label resultLabel; // allows a label
+	private Label promptLabel; // allows a label
+	private TextBox inputBox; // allows a textbox
+	
 	int temp;
 	
 
- void $define() {
+ void $define() { // "main"
  	
-     this.BackgroundColor(COLOR_WHITE);
+     this.BackgroundColor(COLOR_WHITE); //background set to color
      
-     line1 = new HorizontalArrangement(this);
-     line2 = new HorizontalArrangement(this);
-     line3 = new HorizontalArrangement(this);
-     line4 = new HorizontalArrangement(this);
+     line1 = new HorizontalArrangement(this); // places HA on line 1
+     line2 = new HorizontalArrangement(this); // places HA on line 2
+     line3 = new HorizontalArrangement(this); // places HA on line 3
+     
 
      
-     promptLabel = new Label(line1, "Enter a number:");
-     inputBox = new TextBox(line1);
-     inputBox.NumbersOnly(true);
+     promptLabel = new Label(line1, "Enter a number:"); // places label on line 1 "enter a number"
+     inputBox = new TextBox(line1);// places a textbox on line 1
+     inputBox.NumbersOnly(true); // sets textbox ^ to numbers only
    
-     incButton = new Button(line2,"Increment it:"); 
+     incButton = new Button(line2,"Double It:"); // places button on line 2 "double it"
      
-     resultLabel = new Label(line3,"");
+     resultLabel = new Label(line3,""); // places result label on line 3
      
-     outputLabel = new Label(line4, "");
+     
  
-     EventDispatcher.registerEventForDelegation(this, "ButtonClick", "Click");
+     EventDispatcher.registerEventForDelegation(this, "ButtonClick", "Click"); // tells the app what logic/events to look for
     
  } 
  @Override
  public boolean dispatchEvent(Component component, String id, String eventName,
          Object[] args) {
  	
-	    if (component.equals(incButton) && eventName.equals("Click")){
-	    	temp = Integer.parseInt(inputBox.Text());
-	    	temp += 1;
-	    	resultLabel.Text(String.valueOf(temp));
+	    if (component.equals(incButton) && eventName.equals("Click")){ // if statement logic when button clicked
+	    	temp = Integer.parseInt(inputBox.Text()); // takes text from input box multiplys by 2
+	    	temp *= 2;
+	    	resultLabel.Text(String.valueOf(temp)); // takes result of mutiplication and displays it in result label
 	        return true;
 	     } 
 	    
-	    if (component.equals(resultLabel) && eventName.equals("Click")) {
-	    	outputLabel.Text("You pushed me!!");
-	    	return true;
-	    }
+	    
+	    
     return true;
 	} 
 } 
